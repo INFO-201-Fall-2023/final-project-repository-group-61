7c12b7eba9ecf8f51757e173ebd862ef00bbbb9d
Kendrick Echevarria <131416413+kendrickech@users.noreply.github.com>
2023-05-07 23:01:45 -0700

    Update project.R

--- project.R
+++ project.R
@@ -89,3 +89,52 @@ for (name in wanted_col_names) {
 }
 
 df <- na.omit(df)
+
+# Summart Dataset
+summary_df <- df[, 1:200]
+summary_df <- summary_df[-c(84:191)]
+
+summary_df <- mutate(summary_df, TOT_WA = TOT_WA_CHILD + TOT_WA_ADULT + TOT_WA_ELDER)
+summary_df <- relocate(summary_df, TOT_WA, .after=TOT_WA_ELDER)
+
+summary_df <- mutate(summary_df, TOT_BA = TOT_BA_CHILD + TOT_BA_ADULT + TOT_BA_ELDER)
+summary_df <- relocate(summary_df, TOT_BA, .after=TOT_BA_ELDER)
+
+summary_df <- mutate(summary_df, TOT_IA = TOT_IA_CHILD + TOT_IA_ADULT + TOT_IA_ELDER)
+summary_df <- relocate(summary_df, TOT_IA, .after=TOT_IA_ELDER)
+
+summary_df <- mutate(summary_df, TOT_AA = TOT_AA_CHILD + TOT_AA_ADULT + TOT_AA_ELDER)
+summary_df <- relocate(summary_df, TOT_AA, .after=TOT_AA_ELDER)
+
+summary_df <- mutate(summary_df, TOT_NA = TOT_NA_CHILD + TOT_NA_ADULT + TOT_NA_ELDER)
+summary_df <- relocate(summary_df, TOT_NA, .after=TOT_NA_ELDER)
+
+summary_df <- mutate(summary_df, TOT_H = TOT_H_CHILD + TOT_H_ADULT + TOT_H_ELDER)
+summary_df <- relocate(summary_df, TOT_H, .after=TOT_H_ELDER)
+
+demo_sum <- group_by(demo_df, STNAME, CTYNAME)
+demo_sum <- summarise(demo_sum, across(all_of(col_names), sum))
+
+demo_sum_2 <- group_by(demo_df, STNAME)
+demo_sum_2 <- summarise(demo_sum, across(all_of(col_names), sum))
+
+demo_sum_2 <- mutate(demo_sum_2, WA_PCT = TOT_WA / TOT_POP * 100)
+demo_sum_2 <- mutate(demo_sum_2, BA_PCT = TOT_BA / TOT_POP* 100)
+demo_sum_2 <- mutate(demo_sum_2, IA_PCT = TOT_IA / TOT_POP * 100)
+demo_sum_2 <- mutate(demo_sum_2, AA_PCT = TOT_AA / TOT_POP * 100)
+demo_sum_2 <- mutate(demo_sum_2, NA_PCT = TOT_NA / TOT_POP * 100)
+demo_sum_2 <- mutate(demo_sum_2, H_PCT = TOT_H / TOT_POP * 100)
+demo_sum_2 <- demo_sum_2[, -c(2:80)]
+demo_sum_2$state_code <- state.abb[match(demo_sum_2$STNAME, state.name)]
+
+
+summary_df <- full_join(x = summary_df, y = demo_sum_2, by = c('Recip_State' = 'state_code'))
+summary_df <- relocate(summary_df, STNAME, .after = Recip_County)
+
+summary_df <- mutate(summary_df, TOT_POP = TOT_POP_CHILD + TOT_POP_ADULT + TOT_POP_ELDER)
+summary_df <- mutate(summary_df, WA_ABOVE_AVERAGE = (TOT_WA / TOT_POP* 100 >= WA_PCT))
+summary_df <- mutate(summary_df, BA_ABOVE_AVERAGE = (TOT_BA / TOT_POP* 100 >= BA_PCT))
+summary_df <- mutate(summary_df, IA_ABOVE_AVERAGE = (TOT_IA / TOT_POP* 100 >= IA_PCT))
+summary_df <- mutate(summary_df, AA_ABOVE_AVERAGE = (TOT_AA / TOT_POP* 100 >= AA_PCT))
+summary_df <- mutate(summary_df, NA_ABOVE_AVERAGE = (TOT_NA / TOT_POP* 100 >= NA_PCT))
+summary_df <- mutate(summary_df, H_ABOVE_AVERAGE = (TOT_H / TOT_POP* 100 >= H_PCT))
