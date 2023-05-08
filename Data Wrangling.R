a6c926e8d0ee3bdb2878968d52c21da62b85a195
Kendrick Echevarria <131416413+kendrickech@users.noreply.github.com>
2023-05-07 23:09:13 -0700

    Update and rename project.R to Data Wrangling.R

--- Data Wrangling.R
+++ Data Wrangling.R
@@ -1,5 +1,27 @@
 library(readr)
 library(dplyr)
+library(data.table)
+
+
+# reading in datasets
+vacc_df <- fread('county_vaccinations.csv')
+raw_demographics_df <- read_csv("county_demographics.csv")
+
+#filtering dates in demographics df to 1st july 2021
+demographics_df <- filter(raw_demographics_df, YEAR == 3)
+
+# separating date column
+vacc_df$Month <- substr(vacc_df$Date, 1, 2)
+vacc_df$Day <- substr(vacc_df$Date, 4, 5)
+vacc_df$Year <- substr(vacc_df$Date, 7, 10)
+
+# filtering out weekly data from july 2021 in vaccinations df
+days <- c('01', '08', '15', '22', '29')
+july_vacc_df <- filter(vacc_df, Day %in% days, Month == '07', Year == '2021')
+
+# creating new csv files
+write.csv(july_vacc_df, '~/Documents/info201/project/county_vacc_jul2021.csv')
+write.csv(demographics_df, '~/Documents/info201/project/county_demographics_jul2021.csv')
 
 vacc_df <- read.csv("county_vacc_jul2021.csv")
 demo_df <- read.csv("county_demographics_jul2021.csv")
@@ -90,7 +112,7 @@ for (name in wanted_col_names) {
 
 df <- na.omit(df)
 
-# Summart Dataset
+# Summary Dataset
 summary_df <- df[, 1:200]
 summary_df <- summary_df[-c(84:191)]
 
